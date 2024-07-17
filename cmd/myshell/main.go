db4610934f5e8f9f7dc23bd1607fd85b7353b839
Trombitas Imre-Sandor <sandor.trombitas@snyk.io>
2024-07-17 14:35:50 +0300

    exit command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -13,7 +13,18 @@ func main() {
 
 		// Wait for user input
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		command := strings.Split(strings.TrimRight(input, "\n"), " ")[0]
+		command := strings.TrimRight(input, "\n")
+
+		if strings.HasPrefix(command, "exit") {
+			var exitCode int
+
+			_, err := fmt.Sscanf(command, "exit %d", &exitCode)
+			if err == nil {
+				os.Exit(exitCode)
+			}
+
+			os.Exit(0)
+		}
 
 		fmt.Printf("%s: command not found\n", command)
 	}
