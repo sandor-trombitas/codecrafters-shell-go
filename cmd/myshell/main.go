49f77f971e5ddab44704a792dc778a5c48db9b1b
Trombitas Imre-Sandor <sandor.trombitas@snyk.io>
2024-07-17 13:52:07 +0300

    repl

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,12 +8,13 @@ import (
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	command := strings.Split(strings.TrimRight(input, "\n"), " ")[0]
+		// Wait for user input
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		command := strings.Split(strings.TrimRight(input, "\n"), " ")[0]
 
-	fmt.Printf("%s: command not found\n", command)
+		fmt.Printf("%s: command not found\n", command)
+	}
 }
